93984a5effc4d9baf5d435796fd4f9055ffb8a80
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2018-10-02 20:30:43 +0530

    skills charts added

--- src/v2_rishabhrkaushik.github.io/src/components/SkillsComponent/skills.js
+++ src/v2_rishabhrkaushik.github.io/src/components/SkillsComponent/skills.js
@@ -25,11 +25,22 @@ class DonutChart extends Component {
     );
   }
 };
-
 DonutChart.defaultProps = {
   strokewidth: 10,
   size: 120
 };
+
+class BarChart extends Component {
+  render() {
+    return (
+      <div className="bar-container">
+        <div className="bar-skills" style={{width: this.props.value, backgroundColor: this.props.color}}>
+          {this.props.label}
+        </div>
+      </div>
+    );
+  }
+}
 
 class Skills extends Component {
 
@@ -39,23 +50,51 @@ class Skills extends Component {
         <h1> Skills </h1>
         <h2> Top Skills </h2>
         <div id="ringCharts">
-          <DonutChart value={60} valuelabel='Completed'/>
-        </div>
-        <div className="container-fluid">
-          <h2> Other Skills </h2>
           <div className="row">
-            <h3> Programming </h3>
-            <div className="col-md-6">
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
             </div>
-            <h3> Electronics </h3>
-            <div className="col-md-6">
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
+            </div>
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
+            </div>
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
+            </div>
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
+            </div>
+            <div className="col-md-2">
+              <DonutChart value={60} valuelabel='Completed'/>
+            </div>
+          </div>
+        </div>
+        <h2> Other Skills </h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6 skillColumn">
+              <h3> Programming </h3>
+              <BarChart value={'70%'} color="#DD0000" label="HTML" />
+              <BarChart value={'100%'} color="green" label="HTML" />
+              <BarChart value={'10%'} color="blue" label="HTML" />
+            </div>
+            <div className="col-md-6 skillColumn">
+              <h3> Electronics </h3>
+              <BarChart value={'70%'} color="gray" label="HTML" />
+              <BarChart value={'100%'} color="black" label="HTML" />
+              <BarChart value={'10%'} color="#ABDD00" label="HTML" />
             </div>
           </div>
           <div className="row">
-            <h3> Soft Skills </h3>
-            <div className="col-md-6">
+            <div className="col-md-6 skillColumn">
+              <h3> Soft Skills </h3>
+              <BarChart value={'70%'} color="#DD0F0A" label="HTML" />
+              <BarChart value={'100%'} color="#DA00DD" label="HTML" />
+              <BarChart value={'10%'} color="#DDFF00" label="HTML" />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 skillColumn">
             </div>
           </div>
         </div>

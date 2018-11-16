d0306d12b166805df4b1d4fccbc2771e87d39e30
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2018-11-16 23:07:58 +0530

    working till project

--- src/v2_rishabhrkaushik.github.io/src/components/ProjectsComponent/projects.js
+++ src/v2_rishabhrkaushik.github.io/src/components/ProjectsComponent/projects.js
@@ -1,5 +1,5 @@
 import React, {  Component } from 'react';
-import { Collapse, Modal, ModalHeader, ModalBody, ModalFooter, Card, CardText, CardBody, CardHeader, CardFooter, CardDeck} from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Card, CardText, CardBody, CardHeader, CardFooter, CardDeck} from 'reactstrap';
 import "./projects.css";
 import projects from '../../data/projects.json';
 
@@ -59,6 +59,28 @@ class ProjectCard extends Component {
 }
 
 class Projects extends Component {
+
+  makeCardDeck(projects){
+    var json_current_index = 0
+    var card_decks= []
+    while(json_current_index < projects.length)
+    {
+      var project_cards = [];
+      for(var i = 0; i < 4; i++){
+        if(json_current_index + i < projects.length){
+          var project = projects[json_current_index+i];
+          // project_cards.push(<ProjectCard tags={project.tags}/>)
+          project_cards.push(<ProjectCard projectTitle={project.title} projectSubtitle={project.subTitle} tags={project.tags} projectSummary={project.summary} projectDesc={project.projectDesc}/>)
+        }
+        else{
+          project_cards.push(<ProjectCard tags={[]} />)
+        }
+      }
+      card_decks.push(<CardDeck>{project_cards}</CardDeck>)
+      json_current_index = json_current_index + 4;
+    }
+    return <div>{card_decks}</div>
+  }
   render(){
     return (
       <div id="projectsDiv">
@@ -81,9 +103,7 @@ class Projects extends Component {
           } */}
 
             {
-              projects.map(project =>
-                  <ProjectCard projectTitle={project.title} projectSubtitle={project.subTitle} tags={project.tags} projectSummary={project.summary} projectDesc={project.projectDesc}/>
-              )
+              this.makeCardDeck(projects)
             }
           {/* <ProjectCard projectTitle="Project 1" tags="Tag 1, Tag 2, Tag 3, Tag 4" projectSummary="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut tincidunt felis, ut facilisis mi. Integer tristique augue eget eros pharetra mattis. Quisque rutrum scelerisque tellus in condimentum. Sed lacinia ultricies magna, vitae tincidunt magna. Sed elementum ex vel ultrices viverra. Fusce sit amet eleifend metus. Nullam fermentum dapibus ligula finibus semper. Donec vel purus vulputate, faucibus sem eu, eleifend diam. Curabitur sed rutrum lectus. Vivamus mollis mi sit amet erat rutrum, vestibulum ornare nibh hendrerit. "/>
           <ProjectCard projectTitle="Project 2" tags="Tag 1, Tag 2, Tag 3, Tag 4" projectSummary="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut tincidunt felis, ut facilisis mi. Integer tristique augue eget eros pharetra mattis. Quisque rutrum scelerisque tellus in condimentum. Sed lacinia ultricies magna, vitae tincidunt magna. Sed elementum ex vel ultrices viverra. Fusce sit amet eleifend metus. Nullam fermentum dapibus ligula finibus semper. Donec vel purus vulputate, faucibus sem eu, eleifend diam. Curabitur sed rutrum lectus. Vivamus mollis mi sit amet erat rutrum, vestibulum ornare nibh hendrerit. "/>
@@ -94,7 +114,7 @@ class Projects extends Component {
           <ProjectCard projectTitle="Project 5" tags="Tag 1, Tag 2, Tag 3, Tag 4" projectSummary="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut tincidunt felis, ut facilisis mi. Integer tristique augue eget eros pharetra mattis. Quisque rutrum scelerisque tellus in condimentum. Sed lacinia ultricies magna, vitae tincidunt magna. Sed elementum ex vel ultrices viverra. Fusce sit amet eleifend metus. Nullam fermentum dapibus ligula finibus semper. Donec vel purus vulputate, faucibus sem eu, eleifend diam. Curabitur sed rutrum lectus. Vivamus mollis mi sit amet erat rutrum, vestibulum ornare nibh hendrerit. "/>
           <ProjectCard projectTitle="Project 6" tags="Tag 1, Tag 2, Tag 3, Tag 4" projectSummary="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut tincidunt felis, ut facilisis mi. Integer tristique augue eget eros pharetra mattis. Quisque rutrum scelerisque tellus in condimentum. Sed lacinia ultricies magna, vitae tincidunt magna. Sed elementum ex vel ultrices viverra. Fusce sit amet eleifend metus. Nullam fermentum dapibus ligula finibus semper. Donec vel purus vulputate, faucibus sem eu, eleifend diam. Curabitur sed rutrum lectus. Vivamus mollis mi sit amet erat rutrum, vestibulum ornare nibh hendrerit. "/>
           <ProjectCard projectTitle="Project 7" tags="Tag 1, Tag 2, Tag 3, Tag 4, Tag 5, Tag 6,Tag 7, Tag 8, Tag 9, Tag 10" projectSummary="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor." projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut tincidunt felis, ut facilisis mi. Integer tristique augue eget eros pharetra mattis. Quisque rutrum scelerisque tellus in condimentum. Sed lacinia ultricies magna, vitae tincidunt magna. Sed elementum ex vel ultrices viverra. Fusce sit amet eleifend metus. Nullam fermentum dapibus ligula finibus semper. Donec vel purus vulputate, faucibus sem eu, eleifend diam. Curabitur sed rutrum lectus. Vivamus mollis mi sit amet erat rutrum, vestibulum ornare nibh hendrerit. "/>
-          {/* <ProjectCard collapse={true} /> */} */}
+          {/* <ProjectCard collapse={true} /> */}
         </div>
       </div>
     );

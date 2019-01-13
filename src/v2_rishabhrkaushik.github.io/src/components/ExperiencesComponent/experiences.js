a099974c02959c7c67268720732df9a1f13da99b
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-01-13 17:34:16 +0530

    converted experience to bootstrap

--- src/v2_rishabhrkaushik.github.io/src/components/ExperiencesComponent/experiences.js
+++ src/v2_rishabhrkaushik.github.io/src/components/ExperiencesComponent/experiences.js
@@ -1,50 +1,42 @@
 import React, {  Component } from 'react';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 import "./experiences.css";
 import experiences from '../../data/workExperience.json';
 
 class Card extends Component {
   render (){
     return (
-      <article>
-        <div className="inner">
-          <span className="icon">
-            <i className="fa fa-briefcase"></i>
-          </span>
-          <div className="cardHeader">
-            <div className="jobTitle">
-              {this.props.title}
-            </div>
-            <div className="company">
-              {this.props.company}, {this.props.location}
-            </div>
-            <div className="team">
-              {this.props.team}
-            </div>
-            <div className="date">
-              {this.props.startDate} - {this.props.endDate}
-            </div>
-          </div>
-          <p>{this.props.content}</p>
-        </div>
-      </article>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date={this.props.startDate + " - " + this.props.endDate}
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">{this.props.title}</h3>
+          <h4 className="vertical-timeline-element-subtitle">{this.props.team + this.props.company}</h4>
+          {/* <h4 className="vertical-timeline-element-subtitle">{experience.company}</h4> */}
+          <p>
+            {this.props.content}
+          </p>
+        </VerticalTimelineElement>
     );
   }
 }
 
+{/* icon={<i className="fa fa-briefcase"></i>} */}
 class Experiences extends Component {
   render(){
     return (
       <div id="experiencesDiv">
         <h1> Experiences </h1>
-        <section id="timeline">
+        <VerticalTimeline>
           {
             experiences.map((experience) =>
               <Card title={experience.jobTitle} content={experience.jobDesc} company={experience.company} location={experience.location} startDate={experience.startDate} endDate={experience.endDate} team={experience.team} />
             )
           }
-
-          {/* <Card title="Research and Development Intern" content="Did everything"/> */}
-        </section>
+        </VerticalTimeline>
       </div>
     );
   }

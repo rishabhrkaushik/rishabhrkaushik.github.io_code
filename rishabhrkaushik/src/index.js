6e3dbe761e7dcee785b55a88bd510bcc15113acc
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-08-09 09:42:47 +0530

    added particleJS background

--- rishabhrkaushik/src/index.js
+++ rishabhrkaushik/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 // Bulma
 import 'bulma/css/bulma.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('landing'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

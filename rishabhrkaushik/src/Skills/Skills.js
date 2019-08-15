46fa05285ca9e07cee1db402ff3547e8696dd2a1
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-08-15 22:23:45 +0530

    added basic skills component

---
+++ rishabhrkaushik/src/Skills/Skills.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+
+import './Skills.css';
+
+class Skills extends Component {
+  render() {
+    return (
+      <div className="skills-background">
+        <div className="title">
+          Skills
+        </div>
+        <div className="table-container">
+          <table align="center" className="table">
+            <tbody>
+              <tr>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Arduino_Logo.svg" width="200px" alt="Arduino" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.python.org/static/community_logos/python-logo-master-v3-TM-flattened.png" width="200px" alt="Python" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.altexsoft.com/media/2018/07/Product-Management_22.png" width="200px" alt="Project Management" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="http://kalasalingam.ac.in/site/wp-content/uploads/2015/05/Labview-logo.jpg" width="200px" alt="LabView" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.abeautifulsite.net/uploads/2016/04/nodejs.png?width=600&key=e29b3acd7da48dbe62199ba284591dc6e8abd8cb9ce286f5cf89b53a494a9b39" width="200px" alt="NodeJS" />
+                  </figure>
+                </td>
+              </tr>
+              <tr>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Arduino_Logo.svg" width="200px" alt="Arduino" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.python.org/static/community_logos/python-logo-master-v3-TM-flattened.png" width="200px" alt="Python" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.altexsoft.com/media/2018/07/Product-Management_22.png" width="200px" alt="Project Management" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="http://kalasalingam.ac.in/site/wp-content/uploads/2015/05/Labview-logo.jpg" width="200px" alt="LabView" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.abeautifulsite.net/uploads/2016/04/nodejs.png?width=600&key=e29b3acd7da48dbe62199ba284591dc6e8abd8cb9ce286f5cf89b53a494a9b39" width="200px" alt="NodeJS" />
+                  </figure>
+                </td>
+              </tr>
+              <tr>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Arduino_Logo.svg" width="200px" alt="Arduino" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.python.org/static/community_logos/python-logo-master-v3-TM-flattened.png" width="200px" alt="Python" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.altexsoft.com/media/2018/07/Product-Management_22.png" width="200px" alt="Project Management" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="http://kalasalingam.ac.in/site/wp-content/uploads/2015/05/Labview-logo.jpg" width="200px" alt="LabView" />
+                  </figure>
+                </td>
+                <td className="skill-cell">
+                  <figure>
+                    <img src="https://www.abeautifulsite.net/uploads/2016/04/nodejs.png?width=600&key=e29b3acd7da48dbe62199ba284591dc6e8abd8cb9ce286f5cf89b53a494a9b39" width="200px" alt="NodeJS" />
+                  </figure>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Skills;
--- rishabhrkaushik/src/index.js
+++ rishabhrkaushik/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import Landing from './Landing/Landing';
 import Summary from './Summary/Summary';
 import Experience from './Experience/Experience';
+import Skills from './Skills/Skills';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -15,7 +16,7 @@ import 'bulma/css/bulma.css'
 ReactDOM.render(<Landing />, document.getElementById('landing'));
 ReactDOM.render(<Summary  />, document.getElementById('summary'));
 ReactDOM.render(<Experience />, document.getElementById('experience'));
-
+ReactDOM.render(<Skills />, document.getElementById('skills'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

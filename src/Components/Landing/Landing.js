6e647e879bf2e3307c544bc301e86e8079bb7bbd
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2023-09-09 17:36:01 -0500

    added medium instead of quora

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Landing.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faQuora } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 // for fading text
 import ReactTextRotator from 'react-text-rotator';
@@ -163,8 +163,8 @@ function Landing() {
             <a href="https://github.com/rishabhrkaushik" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon className="social-icon" icon={faGithub} />
             </a>
-            <a href="https://www.quora.com/profile/Rishabh-Kaushik-4" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon className="social-icon" icon={faQuora} />
+            <a href="https://medium.com/@rishabhrkaushik" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon className="social-icon" icon={faMedium} />
             </a>
           </div>
         </div>
